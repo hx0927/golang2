a8fccff232a4e6ce9d8225c2cb13883e58d89ac3
胡翔 <939935627@qq.com>
2025-11-16 21:09:08 +0800

    测试

--- 任务1/main.go
+++ 任务1/main.go
@@ -10,16 +10,5 @@ func main() {
 		fmt.Printf("str1 -- index:%d, value:%d\n", index, str1[index])
 	}
 
-	str2 := "测试中文"
-	for index := range str2 {
-		fmt.Printf("str2 -- index:%d, value:%d\n", index, str2[index])
-	}
-	fmt.Printf("len(str2) = %d\n", len(str2))
 
-	runesFromStr2 := []rune(str2)
-	bytesFromStr2 := []byte(str2)
-	fmt.Printf("len(runesFromStr2) = %d\n", len(runesFromStr2))
-
-	
-	fmt.Printf("len(bytesFromStr2) = %d\n", len(bytesFromStr2))
 }
